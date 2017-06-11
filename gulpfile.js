6d295d53625884a60b7907a61410468735f5651a
Ogheneogaga Fidelis Agi <aogaga@gmail.com>
2017-06-10 21:52:58 -0500

    run node install to install & gulp to build and run

---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+const gulp = require('gulp');
+const jade = require('gulp-jade');
+const plumber = require('gulp-plumber');
+const $    = require('gulp-load-plugins')();
+const browserSync =  require('browser-sync').create();
+const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');  
+const uglify = require('gulp-uglify');
+
+
+
+
+
+
+// task to lauch server
+gulp.task('serve', function(){
+    browserSync.init({
+        server: {
+            baseDir: "./build/"
+        }
+    });
+
+});
+
+
+gulp.task('js', function() {
+    return gulp.src('./src/**/*.js')
+        //.pipe(uglify())
+        .pipe(gulp.dest('./build'));
+}); // task to compile javascript
+
+
+
+
+gulp.task('css', function() {
+    gulp.src('src/scss/app.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("./build/css"));
+}); // task to compilte and process csss
+
+
+
+
+// task to compile pug template
+gulp.task('template', function(){
+
+    return gulp.src('src/jade/**/*.pug')
+        .pipe(pug({
+
+            errorHandler: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        }))
+
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest("./build"))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+
+});
+
+
+
+
+gulp.task('default', ['css','js', 'template','serve'], function() {
+    gulp.watch(['./src/scss/**/*.scss'], ['css']);
+    gulp.watch('./src/jade/**/*.pug', ['template']);
+    gulp.watch(['./src/scss/**/*.scss'], browserSync.reload);
+    gulp.watch(['./src/scss/**/*.scss'], browserSync.reload);
+
+});
---
+++ src/js/app.js
@@ -0,0 +1,144 @@
+(function(){
+
+    'use strict';
+    var playCount = 0; 
+    
+    var playerO = [];
+    var playerX = [];
+    
+	const winCombinations = [ 
+							[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], 
+                            [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6] 
+                            ];
+   
+	var Gamecanvas = document.querySelector("#canvas");
+
+	/**
+	 * Handles the click event of the boxes
+	 */
+	Gamecanvas.addEventListener("click", function(e) {
+	//	console.log(e.target.id);
+		playGame(e.target.id);
+		
+	});
+
+
+	/**
+	 *  Starts the game
+	 */
+	function playGame(id) {
+		var box =  document.getElementById(id);
+	
+		if(playCount < 9){
+			
+			
+			if(isPlayerMoveValid(box)){
+				
+			
+				if((playCount % 2) == 0 ){
+					
+					playerX.push(parseInt(id));
+					updateBox(id, "X");
+					checkWins("Player X", playerX);
+						
+				}else{
+					
+					playerO.push(parseInt(id));
+					updateBox(id, "O");	
+					
+					checkWins("Player O", playerO);
+						
+				}
+					
+				playCount++;
+				
+				
+			}else{
+				
+				alertBox({text : "That was an invalid play", class: "error"});
+							
+			}
+			
+			
+		}else{
+			
+			
+			alertBox({text : "Game over no more plays", class: "error"});
+			
+		}
+	
+					
+	}
+
+
+	/**
+	 * updates the screen
+	 */
+
+	function updateBox(elementId, updateText){
+			var box =  document.getElementById(elementId);
+			box.innerText = updateText;
+		
+	}
+	
+	
+	/**
+	 * validates clicks
+	 */
+	function isPlayerMoveValid(elem){
+			if(elem.innerHTML == ""){
+				return true;
+			}
+			
+		return false;	
+	}
+	
+	
+	/**
+	 * udate game status on the screen
+	 */
+	function alertBox(updateInfo){
+		
+		var element = document.getElementById("alert");
+		element.innerHTML = updateInfo.text;
+		element.className = updateInfo.class;
+		
+		setTimeout(function () {
+			element.innerHTML = null;
+			element.className = null;
+		   }, 2000);
+		
+	}
+	
+	/**
+	 * check if a winnder has been found
+	 */
+		
+	function  checkWins(text, playerPlays) {
+	
+		winCombinations.forEach(function(item){
+			if(JSON.stringify(item) === JSON.stringify(playerPlays)){
+				endGame(text);
+			}
+		});
+	}	
+	
+	
+	
+	/**
+	 * 
+	 * Blurs the screen and ends the game
+	 * 
+	 */
+	function endGame(player){	
+		var gamestat = document.getElementById("gamestat");
+		gamestat.innerHTML = player + "  Won the game";	
+									
+		var pageblur = document.getElementById("pageblur");
+		pageblur.className = "pageblur";
+						
+	}
+	
+
+
+})();
